68269b2a2309900c76c9ae9054b4eb14873de276
2myaus <waddlespinespig@gmail.com>
2023-06-09 09:44:02 -0400

    Begin adding utility functions

--- main.c
+++ main.c
@@ -1,6 +1,62 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "misc.h"
+
+bool InBounds(Game game, Square square){
+    return square.row >= 0 && square.col >= 0
+        && square.row < game.game_rules.board_height && square.col < game.game_rules.board_width;
+}
+
+void SetPieceAtPos(UniversalPosition position, int8_t row, int8_t col, Piece piece){
+    position.board[row][col] = piece;
+}
+
+void SetPiece(UniversalPosition position, Square square, Piece piece){
+    SetPieceAtPos(position, square.row, square.col, piece);
+}
+
+Piece GetPieceAtPos(UniversalPosition position, int8_t row, int8_t col){
+    return position.board[row][col];
+}
+
+Piece GetPiece(UniversalPosition position, Square square){
+    return GetPieceAtPos(position, square.row, square.col);
+}
+
+void GetPossiblePieceMoves(Game game, Square square, Move *moves_buf[]){
+    bool moves_like_bishop = false;
+    bool moves_like_rook = false;
+    bool moves_like_knight = false;
+    bool moves_like_king = false;
+    bool moves_like_pawn = false;
+
+    Piece piece = GetPiece(game.position, square);
+
+    switch(piece.type){
+        case Pawn:
+            moves_like_pawn = true;
+            break;
+        case King:
+            moves_like_king = true;
+            break;
+        case Queen:
+            moves_like_bishop = true;
+            moves_like_rook = true;
+            break;
+        case Rook:
+            moves_like_rook = true;
+            break;
+        case Knight:
+            moves_like_knight = true;
+            break;
+        case Bishop:
+            moves_like_bishop = true;
+            break;
+    }
+
+
+}
 
 int main(){
     printf("Piece size: %lu\n", sizeof(Piece));
--- misc.h
+++ misc.h
@@ -4,8 +4,18 @@
 #define MIN_BITS_REQUIRED(value) ((sizeof(value) * 8) - __builtin_clz(value))
 
 #define MAX_BOARD_SIZE 24 //Max board width or height
+
 #define NUM_COLORS 4 //Number of colors supported
 #define NUM_COLOR_BITS MIN_BITS_REQUIRED(NUM_COLORS - 1) //(Subtract 1 for 0-based numbering)
+/*
+Color layout:
+   B
+G     R
+   W
+*/
+
+
+#define STORE_MAX_MOVES 128 //Max number of moves to store before rewriting earlier moves
 
  typedef enum /* : uint8_t*/{
     Pawn,
@@ -24,6 +34,12 @@ typedef enum/* : uint8_t*/{
 } Color;
 
 typedef struct {
+    bool has_king_moved : 1;
+    bool has_king_rook_moved : 1;
+    bool has_queen_rook_moved : 1;
+} ColorData;
+
+typedef struct {
     PieceType type : 5;
     Color color : NUM_COLOR_BITS;
     bool is_royal : 1;
@@ -35,6 +51,11 @@ typedef struct{
 } Square;
 
 typedef struct{
+    Square from;
+    Square to;
+} Move;
+
+typedef struct{
     // --Game-breaking rules--
     uint8_t board_width;
     uint8_t board_height;
@@ -43,10 +64,11 @@ typedef struct{
     bool capture_king; //Capture king to win instead of checkmating
     bool capture_own; //Able to capture your own pieces
     bool sideways_pawns; //Pawns can move sideways
-    bool backwards_pawns;
+    bool backwards_pawns; //Pawns can move backwards
     bool torpedo_pawns; //Pawns can always move 2 spaces forwards
     uint8_t promotion_rank;
     uint8_t check_lives; //For n+ Check modes. Set to 0 to disable
+    bool capture_all; //Capture all pieces to win
     //TODO: Pawn promotion options
     //TODO: Royal Piece option
 
@@ -61,11 +83,15 @@ typedef struct{
 typedef struct{
     Color to_move : NUM_COLOR_BITS;
     Square passantable_square; //Should be set to -1 -1 if no square is available
-    Piece board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+    ColorData color_data[NUM_COLORS]; //Whether the king or rooks have moved
+    Square duck_position;
+    uint8_t num_checks[NUM_COLORS]; //Number of times this color has been checked
+    Piece board[MAX_BOARD_SIZE][MAX_BOARD_SIZE]; //0-based array of pieces in [row][col] order
     Piece captured_pieces[MAX_BOARD_SIZE * MAX_BOARD_SIZE];
-} Position;
+} UniversalPosition;
 
 typedef struct{
     GameRules game_rules;
-    Position position;
+    UniversalPosition position;
+    Move moves[STORE_MAX_MOVES];
 } Game;
